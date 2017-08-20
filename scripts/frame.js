ccfd00cdb0c8e9f64daca1db770431ffe1af5205
T0astBread <m.briedl@live.at>
2017-08-20 12:32:04 +0200

    Started implementing nav
    You can already have menus and sub-menus.

--- scripts/frame.js
+++ scripts/frame.js
@@ -1,5 +1,11 @@
 "use strict";
 $(document).ready(function () {
-    $(".toggle:not([x-toggle])").click(function (evt) { return $(evt.currentTarget).closest(".toggleable").toggleClass("active"); });
-    $(".toggle[x-toggle]").click(function (evt) { return $(evt.currentTarget.getAttribute("x-toggle")).toggleClass("active"); });
+    $(".toggle:not([x-toggle])").click(function (evt) {
+        $(evt.currentTarget).closest(".toggleable").toggleClass("active");
+        evt.preventDefault();
+    });
+    $(".toggle[x-toggle]").click(function (evt) {
+        $(evt.currentTarget.getAttribute("x-toggle")).toggleClass("active");
+        evt.preventDefault();
+    });
 });
--- scripts/frame.ts
+++ scripts/frame.ts
@@ -1,5 +1,14 @@
 $(document).ready(() =>
 {
-    $(".toggle:not([x-toggle])").click(evt => $(evt.currentTarget).closest(".toggleable").toggleClass("active"));
-    $(".toggle[x-toggle]").click(evt => $(evt.currentTarget.getAttribute("x-toggle") as String).toggleClass("active"));
+    $(".toggle:not([x-toggle])").click(evt =>
+    {
+        $(evt.currentTarget).closest(".toggleable").toggleClass("active");
+        evt.preventDefault();
+    });
+
+    $(".toggle[x-toggle]").click(evt =>
+    {
+        $(evt.currentTarget.getAttribute("x-toggle") as String).toggleClass("active");
+        evt.preventDefault();
+    });
 })
